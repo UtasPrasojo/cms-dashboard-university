8e9c71e6ef40b0f6cfb525c35ce66eac60eed4b9
Utas Prasojo <utasprasojo229@gmail.com>
2026-07-13 16:05:49 +0700

    feat:adjust default page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import auth from './auth'
 import dashboard from './dashboard'
 import masterData from './master_data'
+import { useAuthStore } from '@/stores/auth.store'
 
 const arr_routes = [auth,dashboard,masterData]
 const routes = arr_routes.flat(1)
@@ -10,6 +11,15 @@ const routes = arr_routes.flat(1)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to) => {
+  if (to.path !== '/') return
+
+  const authStore = useAuthStore()
+  const isLoggedIn = !!authStore.user?.access_token
+
+  return isLoggedIn ? { name: 'dashboard' } : { name: 'auth.login' }
 })
 
 export default router
